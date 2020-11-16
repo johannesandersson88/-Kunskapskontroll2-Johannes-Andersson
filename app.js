53099f21e2ef0afcaf79c416b0f3c196d4f1eda4
johannesandersson88 <70512019+johannesandersson88@users.noreply.github.com>
2020-11-16 13:23:27 +0100

    Del 2
    Javascript kod skriven.

--- app.js
+++ app.js
@@ -1,2 +1,62 @@
+// Min personliga API key
 const apiKey = '8080185354e3479dfc40e61fc766810c';
-const url = 'api.openweathermap.org/data/2.5/&APPID=8080185354e3479dfc40e61fc766810c'
+
+
+// Hämta lista på städer vid input
+
+let getCity = document.querySelector('.city');
+console.log(getCity)
+
+getCity.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    let cityInput = document.querySelector('#city-search');
+    const citySearch = cityInput.value;
+
+    const cityURL = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&lang=sv&units=metric&appid=${apiKey}`;
+    console.log(cityURL)
+
+    cityInput.value = '';
+    fetch(cityURL).then(
+        function (response) {
+            return response.json();
+        }
+    ).then(
+        function (data) {
+            console.log(data)
+
+            // Hämtar informationen från API beroende på stad.
+            const city = data.name;
+            const status = data.weather[0].description;
+            const temp = data.main.temp;
+            const windSpeed = data.wind.speed;
+            const moist = data.main.humidity;
+            const icon = data.weather[0].icon;
+
+            changeCityHead(city, status, temp, windSpeed, moist, icon)
+        }
+    )
+});
+
+function changeCityHead(ci, st, te, ws, mst, i) {
+    let changeLocation = document.querySelector('.location');
+    changeLocation.innerHTML = ci;
+
+    let changeStatus = document.querySelector('.status');
+    changeStatus.innerHTML = st;
+    changeStatus.style.textTransform = 'capitalize';
+
+    let changeTemperatur = document.querySelector('.temp');
+    changeTemperatur.innerHTML = te + ' °';
+
+    let changeWindSpeed = document.querySelector('.wind');
+    changeWindSpeed.innerHTML = ws + ' m/s';
+
+    let changeMoist = document.querySelector('.moist');
+    changeMoist.innerHTML = mst + ' %';
+
+    changeIcon = document.querySelector('.img');
+    const iconID = i;
+    changeIcon.src = `https://openweathermap.org/img/wn/${iconID}.png`
+
+}
